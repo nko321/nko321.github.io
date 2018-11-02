98299e84e33e8b081305a3c10afc8ce485c2f8e3
Nathan Olberding <nathan.olberding@gmail.com>
2018-11-02 15:15:33 -0700

    Adding files

---
+++ Azure Reset.ps1
@@ -0,0 +1,5 @@
+Get-AzureRmVM | Remove-AzureRmVM -Force
+Get-AzureRmResource -ResourceType Microsoft.Network/networkInterfaces | Remove-AzureRmResource -Force
+Get-AzureRmResource -ResourceType Microsoft.Network/loadBalancers | Remove-AzureRmResource -Force
+Get-AzureRmResource | Remove-AzureRmResource -Force
+Get-AzureRmAdApplication | Remove-AzureRmADApplication -Force
